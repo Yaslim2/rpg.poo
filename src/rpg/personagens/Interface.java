8e06eb8d1a2032db0720486b3807be8ac191ad4c
Emerson1337 <58860863+Emerson1337@users.noreply.github.com>
2021-01-15 00:18:44 -0300

    Update Interface.java

--- src/rpg/personagens/Interface.java
+++ src/rpg/personagens/Interface.java
@@ -1,11 +1,12 @@
 package rpg.personagens;
 
+import java.util.List;
 import rpg.ataquesespeciais.Ataques;
 
 public interface Interface {
 	public TiposDePersonagem getTipo();
+	public void setTipo(TiposDePersonagem tipo);
 	public int getPontosDeVida();
-	public void setPontosDeVida(int pontosDeVida);
 	public int getPoderDeHabilidade();
 	public int getPoderAtaqueFisico();
 	public String getNomeChar();
@@ -13,10 +14,12 @@ public interface Interface {
 	public Ataques getAtaqueEspecial();
 	public Ataques getAtaqueBasico();
 	public int getArmadura();
-	public void setArmadura(int armadura);
 	public int getResistenciaMagica();
-	public void setResistenciaMagica(int resistenciaMagica);
 	public void mostrarDados();
 	public void ataqueEspecial();
+        public void setResistenciaMagica(int resistenciaMagica);
+        public void setArmadura(int armadura);
+        public void setPontosDeVida(int pontosDeVida);
+        public List<Integer> getAtaqueDoAtaqueEspecial();
     public void powerUp();
 }
