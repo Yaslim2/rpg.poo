935a5e58c3a53134ab877638a6b4b1a8f8a14171
Emerson1337 <58860863+Emerson1337@users.noreply.github.com>
2021-01-15 20:24:33 -0300

    Batalha finalizada

--- src/rpg/batalha/Batalha.java
+++ src/rpg/batalha/Batalha.java
@@ -14,7 +14,7 @@ public class Batalha {
 
     public Batalha(List<Personagem> personagens) {
         this.lutadores = (List<Personagem>) personagens;
-        
+        //LISTANDO TODOS OS GUERREIROS CARREGADOS NO VETOR LUTADORES
         System.out.println("\nOS GUERREIROS A LUTAR SERÃO:\n");
         for(Personagem i: lutadores){
             System.out.println(i.getNomeChar() + " -> " + i.getTipo() + "\n");
@@ -31,6 +31,7 @@ public class Batalha {
          System.out.println("\nDeseja iniciar a luta?\n1 - Iniciar.\n2 - Cancelar.");
          Scanner entrada = new Scanner(System.in);
          int op = entrada.nextInt();
+         //VERIFICANDO SE O USUARIO NAO FUGIU DO ESCOPO DA PERGUNTA
          while(true){
              if(op <= 2 && op >0){
                 break;
@@ -39,8 +40,11 @@ public class Batalha {
                 }
                 op = entrada.nextInt();
          }
+         
+         //SWITCH CASE PRINCIPAL, CONTENDO MAIS 5 OUTROS SWITCH CASE, BASEADOS NA IDEIA DO JOGO
         switch(op){
             case 1:
+                //VERIFICANDO SE EXISTEM MAIS PERSONAGENS DO QUE 1 PARA INICIAR A BATALHA, SE NÃO, A FUNÇÃO BATALHAR É QUEBRADA
                 if(lutadores.size() <= 1){
                     System.out.println("\nGuerreiros insuficientes para iniciar a batalha. Retornando ao lobby...\n\n");
                     break;
@@ -49,16 +53,21 @@ public class Batalha {
                 System.out.println("READY?");
                 int rounds = 1;
                 
+                //LOOP PARA ENGLOBAR TODOS OS ROUNDS COM CONDIÇÃO DE QUEBRA BASEADA EM UM IF QUE VERIFICA O TAMANHO DO VETOR DOS LUTADORES.
+                //SE O VETOR LUTADORES TIVER UM SIZE == 1, ENTAO, SO RESTOU UM GUERREIRO VIVO, LOGO, ELE É O CAMPEÃO.
                 while(true){
                     int tamanho = lutadores.size() - 1;
-                    
+                    //IF QUE QUEBRA O WHILE TRUE
                     if(tamanho == 0){
                         break;
                     }
+                    
                     System.out.println("\n-------------- *ROUND* " + rounds + " --------------");
+                    //FUNÇÃO DE SORTEIO
                     Collections.shuffle(lutadores); 
                     System.out.println("\nSorteando a ordem de jogada: ");
                     
+                    //LISTANDO TODOS OS GUERREIROS *SORTEADOS*
                     for(int i = 0; i <= tamanho; i++){
                         System.out.println(i+1 + " - " + lutadores.get(i).getNomeChar());
                     }
@@ -66,7 +75,9 @@ public class Batalha {
                     System.out.println("Pressione ENTER para continuar...");
                     new java.util.Scanner(System.in).nextLine();
                     
+                    //FOR RESPONSÁVEL POR FAZER COM QUE CADA GUERREIRO JOGUE NO ROUND
                     for(int i = 0; i <= tamanho; i++){ // round ja iniciado
+                      if(lutadores.get(i).getPontosDeVida() > 0){  
                         System.out.println("É a vez do guerreiro " + lutadores.get(i).getNomeChar());
                         System.out.println("O que ele fará?\n\n1 - Atacar um guerreiro.\n2 - Aprimorar Defesa.\n3 - PowerUp (Carregar habilidade especial).");
                         int atacado = entrada.nextInt();
@@ -79,13 +90,26 @@ public class Batalha {
                                    atacado = entrada.nextInt();
                                 }
                         
+                        //VERIFICANDO QUAL OPÇÃO O GUERREIRO SELECIONOU
                         switch(atacado){
-                            //atacar
+                            //ATAQUE
                             case 1:
                                 System.out.println("Qual outro guerreiro este " + lutadores.get(i).getTipo() + " irá atacar?\n");
                                 //removendo o guerreiro que irá atacar do vetor e para listar os suscetives ao ataque
                                 List <Personagem> listaDeGuerreiros = new ArrayList<>(lutadores); 
                                 listaDeGuerreiros.remove(i);
+                                
+                                //VERIFICANDO SE EXISTE ALGUM GUERREIRO MORTO PARA NAO SER OPÇÃO DE ATAQUE
+                                try{
+                                for(int o = 0; o <= listaDeGuerreiros.size(); o++){
+                                    if(listaDeGuerreiros.get(o).getPontosDeVida() <= 0){
+                                        listaDeGuerreiros.remove(o);
+                                        break;
+                                    }
+                                }}
+                                catch(Exception e){
+                                    //apenas ignroando o erro que dará de index
+                                }
                                 int sizeGuerreiros = listaDeGuerreiros.size();
                                 
                                 //listando guerreiros suscetiveis ao ataque
@@ -105,12 +129,15 @@ public class Batalha {
                                        }
                                        attack = entrada.nextInt();
                                     }
-                                
-                                Personagem sobAtaque = listaDeGuerreiros.get(attack-1);
+                                    
+                                    //ARMAZENANDO O GUERREIRO QUE SERÁ ATACADO BASEADO NA ESCOLHA DO INDEX DENTRO DO VETOR LISTADEGUERREIROS
+                                    //DECLARADO NA LINHA 98
+                                    Personagem sobAtaque = listaDeGuerreiros.get(attack-1);
                                         
                                     System.out.println("Qual ataque você quer realizar?\n1 - Ataque Padrão\n2 - Ataque Especial\n");
                                     int typeOfAtt = entrada.nextInt();
                                     
+                                    //VERIFICANDO SE A RESPOSTA DO USUÁRIO NAO FUGIU DO ESCOPO DO PROGRAMA
                                     while(true){
                                         if(typeOfAtt <= 2 && typeOfAtt >0){
                                             break;
@@ -119,7 +146,9 @@ public class Batalha {
                                         }
                                         typeOfAtt = entrada.nextInt();
                                     }
+                                    
                                     switch(typeOfAtt){
+                                        //ATAQUE PADRÃO
                                         case 1:
                                             //compensar os ataques nas vidas
                                             //exibir os status
@@ -134,6 +163,7 @@ public class Batalha {
                                                 tipoAtaquePadrao = entrada.nextInt();
                                             }
                                             switch(tipoAtaquePadrao){
+                                                //ATAQUE PADRÃO COM DANO FÍSICO
                                                 case 1:
                                                     System.out.println("\n\n" + lutadores.get(i).getNomeChar() + " ESTÁ ATACANDO " + sobAtaque.getNomeChar() + " COM SEU ATAQUE " + sobAtaque.getAtaqueBasico() + "\n");
                                                     int verif = (lutadores.get(i).getPoderAtaqueFisico() - sobAtaque.getArmadura());
@@ -143,6 +173,7 @@ public class Batalha {
                                                         System.out.println("\n\n" + sobAtaque.getNomeChar() + " RESISTIU AO ATAQUE DEVIDO A SUA ARMADURA!\n");
                                                     }
                                                     break;
+                                                //ATAQUE PADRÃO COM PODER DE HABILIDADE
                                                 case 2:
                                                     System.out.println("\n\n" + lutadores.get(i).getNomeChar() + " ESTÁ ATACANDO COM MAGIA O(A) " + sobAtaque.getNomeChar() + "\n");
                                                     int verif2 = (lutadores.get(i).getPoderDeHabilidade() - sobAtaque.getResistenciaMagica());
@@ -163,6 +194,7 @@ public class Batalha {
                                             System.out.println("\nATACANTE:\n");
                                             lutadores.get(i).mostrarDados();
                                             System.out.println("\nATACADO:\n");
+                                            //VERIFICANDO SE O JOGADOR SOBATAQUE MORREU, LOGO, OBTEVE UMA VIDA NEGATIVA OU IGUAL A 0
                                             if(sobAtaque.getPontosDeVida() <= 0){
                                                     System.out.println(sobAtaque.getNomeChar() + " NÃO RESISTIU AO ATAQUE E FOI DESSA PRA MELHOR! PRESS F\n");
                                                 } else {
@@ -170,18 +202,19 @@ public class Batalha {
                                             }
                                             System.out.println("\n******************************");
                                             break;
-                                            
+                                        //ATAQUE ESPECIAL  
                                         case 2:
                                             //compensar os ataques nas vidas
                                             //exibir os status
-                                            //VERIFICAR SE ESTÁ COM A ULT PRONTA SE NAO, ATAQUE NORMAL
-                                            //lutadores.get(i).ataqueEspecial();
+                                            //VERIFICAR SE ESTÁ COM A ULT PRONTA SE NAO, OBRIGA O USUARIO A REALIZAR UM ATAQUE PADRÃO PELA FUNÇÃO CATCH(TRY)
                                             
                                             // 0 - poderAtaqueFisicoEspecial
                                             // 1 - poderDeHabilidadeEspecial
-                                            
                                                 System.out.println("\n\n" + lutadores.get(i).getNomeChar() + " TENTARÁ APRIMORAR SEUS ATRIBUTOS E IRÁ ATACAR " + sobAtaque.getNomeChar() + " COM SEU ATAQUE " + sobAtaque.getAtaqueEspecial());
                                                 lutadores.get(i).ataqueEspecial();
+                                                
+                                                //TENTARÁ ACESSAR O VETOR RETORNADO DO ATAQUE ESPECIAL, CASO NÃO HAJA DADOS NO VETOR, O ATAQUE ESPECIAL
+                                                //NÃO ACONTECEU, LOGO, CAIRÁ NO CATCH
                                                 try {
                                                 int verificacao = (lutadores.get(i).getAtaqueDoAtaqueEspecial().get(0) - sobAtaque.getArmadura());
                                                 int verificacao2 = (lutadores.get(i).getAtaqueDoAtaqueEspecial().get(1) - sobAtaque.getResistenciaMagica());
@@ -196,6 +229,8 @@ public class Batalha {
                                                 } else {
                                                     System.out.println("\n\n" + sobAtaque.getNomeChar() + " RESISTIU AO ATAQUE MÁGICO DEVIDO A SUA RESISTÊNCIA MÁGICA!\n");
                                                 }
+                                                //AQUI INFORMAREMOS QUE O USUÁRIO NAO TEM PONTOS DE HABILIDADE SUFICIENTE PARA REALIZAR O ATAQUE ESPECIAL
+                                                //DANDO A OPÇÃO DE O MESMO ESCOLHER O TIPO DE ATAQUE PADRÃO.
                                                 } catch(Exception e) {
                                                     System.out.println("Sem pontos de habilidade para realizar o Ataque Especial. Realize o ataque padrão!\n1 - Ataque Padrão\n");
                                     
@@ -252,6 +287,7 @@ public class Batalha {
                                                 System.out.println("Pressione ENTER para continuar...");
                                                 new java.util.Scanner(System.in).nextLine();
                                                 
+                                                //EXIBINDO OS DANOS E PONTOS DE HABILIDADE
                                                 System.out.println("\n******************************");
                                                 System.out.println("\n\nSTATUS DOS GUERREIROS APÓS ATAQUE ESPECIAL: ");
                                                 System.out.println("\nATACANTE:\n");
@@ -273,7 +309,7 @@ public class Batalha {
                                     new java.util.Scanner(System.in).nextLine();
                                     
                                     break;
-                            //defesa
+                            //APRIMORANDO DEFESA
                             case 2:
                                 System.out.println("Qual tipo de defesa deseja aprimorar?\n\n1 - Resistência Mágica\n2 - Armadura\n");
                                 
@@ -311,7 +347,7 @@ public class Batalha {
                                 new java.util.Scanner(System.in).nextLine();
                                 
                                 break;
-                            //POWERUP    
+                            //REALIZANDO POWERUP    
                             case 3:
                                 System.out.println("\n*****O guerreiro " + lutadores.get(i).getNomeChar() + " está upando suas habilidades!*****\n");
                                 lutadores.get(i).powerUp();
@@ -323,63 +359,51 @@ public class Batalha {
                                 
                                 break;
                             }
-                        
-                        
-                        
-                        //removendo os guerreiros mortos da batalha
-                       // for(int k=0; k<= tamanho; k++){
-                            if(lutadores.get(i).getPontosDeVida() <= 0){
-                                lutadores.get(i).setNomeChar(lutadores.get(i).getNomeChar() + " (MORTO!)");
-                            }
-                        //}
-                        
+                        } else {
+                            //TENDO EM VISTA QUE SÓ É POSSÍVEL
+                            //MORRER UM GUERREIRO POR ROUND, O FOR É QUEBRADO.
+                            System.out.println("\nSeria a vez do guerreiro " + lutadores.get(i).getNomeChar() + ", mas ele era fraco demais e faleceu. Press f to pay respect!\nPulando a vez!\n\n");
+                            System.out.println("Pressione ENTER para continuar...");
+                            new java.util.Scanner(System.in).nextLine();
                         }
-                        int tamanhoRemocao = lutadores.size() - 1;
-                        
-                        try{
+                    }
+                    //INCREMENTANDO OS ROUNDS. O NÚMERO DE ROUNDS É INDETERMINADO POIS A BATALHA SÓ ACABA QUANDO RESTA
+                    //UM GUERREIRO VIVO
+                    rounds++;   
+                    int tamanhoRemocao = lutadores.size() - 1;
                         for(int k=0; k<= tamanhoRemocao; k++){
                             if(lutadores.get(k).getPontosDeVida() <= 0){
                                 lutadores.remove(lutadores.get(k));
+                                break;
+                                }
                             }
-                        }}
-                        catch(Exception e){
-                        }
-                        
-                        rounds++;
-                        
-                    }
+                }
                 
                    // restaurar(backup);
-                    
-
-
                 break;
-                
+            //CANCELANDO A BATALHA    
             case 2:
             	System.out.println("Saindo para o lobby...");
                 System.out.println("Pressione ENTER para continuar...");
                 new java.util.Scanner(System.in).nextLine();
             	break;
-                
+            //QUALQUER COISA DIFERENTE DO INTERVALO DA PERGUNTA, O QUE RETORNA PARA O LOBBY    
             case 0:
                 System.out.println("Comando inválido, retornando ao lobby...");
                 System.out.println("Pressione ENTER para continuar...");
                 new java.util.Scanner(System.in).nextLine();
                 break;
         	}
-         
+        //CASO HAJA APENAS UM PERSONAGEM NO VETOR, ELE FOI O CAMPEÃO, LOGO, AQUI A SUA APRESENTAÇÃO.
         if(lutadores.size() == 1){
-            System.out.println("AEEEEEEE. O(A)" + lutadores.get(0).getNomeChar() + " FOI O GRANDE VENCEDOR!!!!!");
+            System.out.println("\n\n\n\n");
+            System.out.println("\n**************CAMPEÃO****************\n");
+            System.out.println("O(A)" + lutadores.get(0).getNomeChar() + " FOI O GRANDE CAMPEÃO!!!!!\n");
+            System.out.println("\n******************************");
             System.out.println("Pressione ENTER para continuar...");
                 new java.util.Scanner(System.in).nextLine();
         }
     }
-    
-    
-    public void lutar(){
-        
-    }
-    
     
     public void restaurar(List p){
         this.lutadores = p;
